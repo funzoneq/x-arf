e2fcfbb87469979bb63a2414aaa873b74eb346fb
funzoneq <arnoud@tumblr.com>
2013-01-22 12:47:15 +0100

    Test all the tests

--- regex.py
+++ regex.py
@@ -1,3 +1,4 @@
+import os
 import re
 from yaml import load
 
@@ -6,13 +7,14 @@ try:
 except ImportError:
     from yaml import Loader
 
-with open('tests/20130120173004-0.eml') as f:
-	data = f.readlines()
-	data = "".join(data)
+for files in os.listdir("tests/"):
+	with open('tests/%s' % files) as f:
+		data = f.readlines()
+		data = "".join(data)
 
-	pat = re.compile(r"^(---\n.*?)\n\n", re.S|re.M)
-	result = pat.findall(data)
+		pat = re.compile(r"^(---\n.*?)\n\n", re.S|re.M)
+		result = pat.findall(data)
 
-	if len(result) > 0:
-		xarf = load(result[0], Loader=Loader)
-		print xarf
+		if len(result) > 0:
+			xarf = load(result[0], Loader=Loader)
+			print xarf
